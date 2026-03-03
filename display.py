c90ca3e4ed6aaa3b39a8b63db4265aee9fd9099f
LtCouchCammander <149845440+LtCouchCammander@users.noreply.github.com>
2026-03-03 10:20:12 -0600

    Modify scatter_plot to include charge and limits
    Updated scatter_plot function to include charge parameter and adjusted plot limits.

--- display.py
+++ display.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 
 def scatter_plot(x, y, charge):
     '''
-    scatter_plot(x, y)
+    scatter_plot(x, y) | 
+    oks so i believe the particals will have x and y cordinates and then a charge level to indicate which side of the graph it will be on, or idk how that will effect the x axis... maybe the charge is the x axis??? ¯\_(ツ)_/¯  idk will see later on ima just start writing stufff.
     
-    oks so i believe the particals will have x and y cordinates and then a charge level to indicate which side of the graph
-    it will be on, or idk how that will effect the x axis....
-    maybe the charge is the x axis??? ¯\_(ツ)_/¯  idk will see later on ima just start writing stufff.
-    
-    input x and y cordinates along with the charge level and returns the plot
+    input x and y cordinates along with the charge level and shoes the plot
     '''
     plt.scatter(x, y)
+    plt.xlim(0, 1)
     plt.title("Random Walk Project\n Simulating Particals In A Biased Electrical Field")
     plt.xlabel("Charge Level\n 0 being positive and 1 being negative")
     plt.ylabel("wooaah y axis!!!")
     plt.show()
+    
+    #need to work on like colours 
