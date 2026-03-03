ece4075510951e5833c15178a3b4cbaaafd1274c
LtCouchCammander <149845440+LtCouchCammander@users.noreply.github.com>
2026-03-03 09:56:46 -0600

    Implement scatter_plot function in display.py
    Initial Function creation.
    Added a scatter_plot function to plot particles based on their coordinates and charge level.

--- display.py
+++ display.py
@@ -1,8 +1,12 @@
-import madplotlib.plot as plt
+import matplotlib.pyplot as plt
 
-
-
-# for each frame of animation
-    # plots the graph of a single frame 
-
-
+def scatter_plot(x, y, charge):
+    '''
+    scatter_plot(x, y, charge)
+    
+    oks so i believe the particals will have x and y cordinates and then a charge level to indicate which side of the graph
+    it will be on, or idk how that will effect the x axis....
+    maybe the charge is the x axis??? ¯\_(ツ)_/¯  idk will see later on ima just start writing stufff.
+    
+    input x and y cordinates along with the charge level and returns the plot
+    '''
