f4156981b35b9f8c3cbaf4c06cfc07219c5de7f2
Hans <hans79915@gmail.com>
2026-03-04 21:55:08 -0600

    idk what i should specifically put for the display graph x and y labels. I also made a main.py temporarily to see if things work, which I think they do. It generates the graphs and looks like the particles are movin and stuff. Not quite sure how simple turning it into a gif is gonna be since the gifs would be for each particle, or unless its supposed to be all three or whatever number of particles together on the graph and the gif shows their movements. which makes more sense as im typing this out... idk ima save dis and take a lil break :)

--- display.py
+++ display.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 
-def scatter_plot(partical):
+def scatter_plot(particle, x_range, y_range):
     '''
-    input list with first index being charge level, and second index being another list containing partical cordinates
+    input list with first index being charge level, and second index being another list containing particle coordinates. Along with that input the x and y ranges.
 
     creates scatter plot for every coordinate in found in the second index of the inputted list
     '''
-    if partical[0] == 1:
+    # Determines particle colour based on charge
+    if particle[0] == 1:
         colour = "red"
     else:
         colour = "black"
-    
-    partical_cordinates = partical[1]
-    for i in partical_cordinates:
+
+    # Creates a plot for each coordinate the partical has been through
+    particle_coordinates = particle[1]
+    for i in particle_coordinates:
         cord = i
-        plt.scatter(cord[0], cord[1], c = colour)
+        plt.xlim(x_range[0], x_range[1])
+        plt.ylim(y_range[0], y_range[1])
+        plt.scatter(cord[0], cord[1], c=colour)
         plt.title("Random Walk Project\n Simulating Particals In A Biased Electrical Field")
         plt.xlabel("insert text here")
         plt.ylabel("wooaah y axis!!!")
         plt.show()
-
-
-listo = [1, [[3.15, 4.95], [3.15, 4.95], [3.15, 4.95], [3.15, 4.95], [3.15, 4.95], [3.15, 4.95], [3.15, 4.95], [3.15, 4.95], [3.15, 4.95], [3.15, 4.95]]]
-scatter_plot(listo)
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # Temporary main testing, can delete later if wanted :)
+
+import simulation
+import display
+
+print("Simulation of Charged Particles Moving Throughout a Biased Electrical Field")
+print("Please input the following parameters:")
+n = int(input("Number of particles: "))
+step_size = int(input("Length of particle step: "))
+frames = int(input("Number of animation frames: "))
+x_range = [int(input("Field minimum x coordinate range: ")), int(input("Field maximum x coordinate range: "))]
+y_range = [int(input("Field minimum y coordinate range: ")), int(input("Field maximum y coordinate range: "))]
+
+result = simulation.run_simulation(n, step_size, frames, x_range, y_range)
+
+for i in result:
+    display.scatter_plot(i, x_range, y_range)
