cadc362c777aa0c6654ca1ae45b77b07e8a425b8
Hans <hans79915@gmail.com>
2026-03-06 02:23:17 -0600

    Refinements to main

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # Temporary main testing, can delete later if wanted :)
-
 import simulation
 import display
 
-print("Simulation of Charged Particles Moving Throughout a Biased Electrical Field")
-print("Please input the following parameters")
-n = int(input("Number of particles: "))
-step_size = int(input("Length of particle step: "))
-frames = int(input("Number of animation frames: "))
-x_range = [int(input("Field minimum x coordinate range: ")), int(input("Field maximum x coordinate range: "))]
-y_range = [int(input("Field minimum y coordinate range: ")), int(input("Field maximum y coordinate range: "))]
+def main():
+    print("Simulation of Charged Particles Moving Throughout a Biased Electrical Field")
+    print("Please input the following parameters")
+    n = int(input("Number of particles: "))
+    step_size = int(input("Length of particle step: "))
+    frames = int(input("Number of animation frames: "))
+    x_range = [int(input("Electric Field, x coordinate range\nminimum: ")), int(input("maximum: "))]
+    y_range = [int(input("Electric Field, y coordinate range\nminimum: ")), int(input("maximum: "))]
 
-generated_particles = simulation.run_simulation(n, step_size, frames, x_range, y_range)
+    generated_particles = simulation.run_simulation(n, step_size, frames, x_range, y_range)
 
-display.scatter_plot(generated_particles, frames, x_range, y_range)
+    display.scatter_plot(generated_particles, frames, x_range, y_range)
+
+if __name__ == "__main__":
+    main()
